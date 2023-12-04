3a21bcb8f938c7029d8fe5b5b4ddb71e69d47952
Grace Campbell <gcampbell@doximity.com>
2023-12-04 17:21:43 -0500

    I keep naming tests wrong

--- advent_2023/test/test_day_02.py
+++ advent_2023/test/test_day_02.py
@@ -7,9 +7,9 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".split('\n')
 
 
-def test_day_01():
+def test_part_one():
     assert part_one(inputs) == 8
 
 
-def test_day_02():
+def test_part_two():
     assert part_two(inputs) == 2286
